372008b6f30a99c9fdb6bd3b701fddcfeed8b77c
Diplomatictw0 <j.cuervoo@uniandes.edu.co>
2024-04-15 09:35:13 -0500

    Envio

--- tsbasico/docs/main.js
+++ tsbasico/docs/main.js
@@ -28,7 +28,7 @@ var series = [
     { id: 3, name: "Game of Thrones", channel: "HBO", seasons: 7 },
     { id: 4, name: "The Big Bang Theory", channel: "CBS", seasons: 12 },
     { id: 5, name: "Sherlock", channel: "BBC", seasons: 4 },
-    { id: 6, name: "A Very English Scandal", channel: "BBC", seasons: 2 }
+    { id: 6, name: "A Very English Scandal", channel: "BBC", seasons: 2}
 ];
 
 mostrarSeries(series);
